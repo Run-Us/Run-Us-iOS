50d55ae29edd508516a126a42145530332d5ed02
slr-09 <ge12090@naver.com>
2024-08-31 18:07:34 +0900

    project setting

---
+++ RunUs/RunUs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunUs
+//
+//  Created by 가은 on 8/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunUs/RunUs/RunUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunUsApp.swift
+//  RunUs
+//
+//  Created by 가은 on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct RunUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
