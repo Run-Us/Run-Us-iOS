0abf3e8baf448155dad3af8b0f5c872baf15816c
slr-09 <ge12090@naver.com>
2024-09-04 23:56:26 +0900

    💄 Design: LoginPage 구성

---
+++ RunUs/RunUs/View/LoginPage.swift
@@ -0,0 +1,30 @@
+//
+//  LoginPage.swift
+//  RunUs
+//
+//  Created by 가은 on 9/4/24.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 24) {
+            Text("Run with Us!")
+                .font(.title1)
+            Text("모든 러너가 즐겁게 성장하며\n함께 달릴 수 있는 러닝 크루 플랫폼")
+                .font(.body1)
+                .lineSpacing(0.8)
+            Button(action: {}, label: {
+                Image("kakao_login_button")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            })
+        }
+        .padding(.horizontal, 20)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
