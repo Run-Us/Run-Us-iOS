cca00663e12d40433580315547d29e696b5f92c6
slr-09 <ge12090@naver.com>
2024-09-05 22:47:42 +0900

    ✨ Feat: 카카오톡 로그인을 위한 설정
    kakao sdk 초기화, 카카오톡에서 서비스 앱으로 돌아왔을 때 정상적으로 처리하도록 설정

--- RunUs/RunUs/RunUsApp.swift
+++ RunUs/RunUs/RunUsApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct RunUsApp: App {
+    
+    init() {
+        // kakao sdk 초기화
+        let kakaoNativeAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey: kakaoNativeAppKey as! String)
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginPage()
+                .onOpenURL(perform: { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                })
         }
     }
 }
