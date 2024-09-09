5cd34eb0fedf28357dc4bb50a2cf1e04b110d0bf
jikbb2 <jikbb2@gmail.com>
2024-09-09 21:42:52 +0900

    💄 Add: HomePage 생성

---
+++ RunUs/RunUs/View/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  RunUs
+//
+//  Created by byeoungjik on 9/9/24.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
