c29e0fec02c9bbc1354c5d388f31e057c1d71a18
jikbb2 <jikbb2@gmail.com>
2024-09-09 21:44:05 +0900

    💄 Add: CrewPage 생성

---
+++ RunUs/RunUs/View/CrewPage.swift
@@ -0,0 +1,18 @@
+//
+//  CrewPage.swift
+//  RunUs
+//
+//  Created by byeoungjik on 9/9/24.
+//
+
+import SwiftUI
+
+struct CrewPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CrewPage()
+}
