bf3242d8fd1710d8374a6c21f232c8365cd6b6fc
jikbb2 <jikbb2@gmail.com>
2024-09-09 21:45:41 +0900

    💄 Add: RunningPage 생성

---
+++ RunUs/RunUs/View/RunningPage.swift
@@ -0,0 +1,18 @@
+//
+//  RunningPage.swift
+//  RunUs
+//
+//  Created by byeoungjik on 9/9/24.
+//
+
+import SwiftUI
+
+struct RunningPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RunningPage()
+}
