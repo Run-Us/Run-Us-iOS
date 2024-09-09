65406b38ad3753c75e928032d2a0f79d1e818f4d
jikbb2 <jikbb2@gmail.com>
2024-09-09 21:44:35 +0900

    💄 Add: MyPage 생성

---
+++ RunUs/RunUs/View/MyPage.swift
@@ -0,0 +1,18 @@
+//
+//  MyPage.swift
+//  RunUs
+//
+//  Created by byeoungjik on 9/9/24.
+//
+
+import SwiftUI
+
+struct MyPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPage()
+}
