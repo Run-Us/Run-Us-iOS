9c7ad4c0148cd074412a113d73c90efd0886dbec
slr-09 <ge12090@naver.com>
2024-09-13 14:59:56 +0900

    ✨ Feat: 로그인 성공 시 메인 화면으로 전환

--- RunUs/RunUs/View/LoginPage.swift
+++ RunUs/RunUs/View/LoginPage.swift
@@ -9,25 +9,31 @@ import SwiftUI
 
 struct LoginPage: View {
     @StateObject var authVM: AuthViewModel = AuthViewModel()
+    @State private var loginSuccess: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 24) {
-            Text("Run with Us!")
-                .font(.title1)
-            Text("모든 러너가 즐겁게 성장하며\n함께 달릴 수 있는 러닝 크루 플랫폼")
-                .font(.body1)
-                .lineSpacing(0.8)
-            Button(action: {
-                authVM.kakaoLogin { isSuccess in
-                    
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 24) {
+                Text("Run with Us!")
+                    .font(.title1)
+                Text("모든 러너가 즐겁게 성장하며\n함께 달릴 수 있는 러닝 크루 플랫폼")
+                    .font(.body1)
+                    .lineSpacing(0.8)
+                Button(action: {
+                    authVM.kakaoLogin { isSuccess in
+                        loginSuccess = isSuccess
+                    }
+                }, label: {
+                    Image("kakao_login_button")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                })
+                .navigationDestination(isPresented: $loginSuccess) {
+                    MainPage()
                 }
-            }, label: {
-                Image("kakao_login_button")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            })
+            }
+            .padding(.horizontal, 20)
         }
-        .padding(.horizontal, 20)
     }
 }
 
