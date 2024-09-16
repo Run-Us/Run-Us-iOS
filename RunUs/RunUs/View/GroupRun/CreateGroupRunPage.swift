f63ee3fb837c7a0219d66eabc85b4bd9b12274fe
jikbb2 <jikbb2@gmail.com>
2024-09-16 18:29:22 +0900

    💄 Add: 그룹 생성하기 대기방 뷰 생성

---
+++ RunUs/RunUs/View/GroupRun/CreateGroupRunPage.swift
@@ -0,0 +1,52 @@
+//
+//  CreateGroupRunPage.swift
+//  RunUs
+//
+//  Created by byeoungjik on 9/16/24.
+//
+
+import SwiftUI
+
+struct CreateGroupRunPage: View {
+    let screenWidth = UIScreen.main.bounds.size.width
+    @Binding var noticeContent: String?
+    @State var noticeBar = NoticeBar(noticeContent: .constant("러너에게 아래 인증번호를 알려주세요"))
+    var body: some View {
+        ZStack {
+            VStack {
+//                알림창
+                noticeBar
+                    .frame(width: screenWidth, height: 40)
+                    .padding()
+//                인증번호
+                Text("인증번호")
+                Text("6531")
+                    .font(.system(size: 82, weight: .bold))
+//                참가자
+                List {
+//                    mokup
+                    HStack {
+                        Circle()
+                            .foregroundColor(.pink)
+                            .frame(width:40, height: 40)
+                        Text("조성훈")
+                            .padding()
+                        Spacer()
+                        Circle()
+                            .foregroundColor(.green)
+                            .frame(width:40, height: 40)
+                        Text("조성훈")
+                            .padding()
+                    }
+//                    .listRowBackground(.clear)
+                }
+                .listRowSeparator(.hidden)
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    CreateGroupRunPage(noticeContent: .constant("러너에게 아래 인증번호를 알려주세요"))
+}
