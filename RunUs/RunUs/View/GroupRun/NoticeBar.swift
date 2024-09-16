62b1f6374866e5f443ad23e7c7c2c8552451de6f
jikbb2 <jikbb2@gmail.com>
2024-09-16 18:26:59 +0900

    💄 Add: Notice bar 컴포넌트 생성

---
+++ RunUs/RunUs/View/GroupRun/NoticeBar.swift
@@ -0,0 +1,32 @@
+//
+//  NoticeBar.swift
+//  RunUs
+//
+//  Created by byeoungjik on 9/16/24.
+//
+
+import SwiftUI
+
+struct NoticeBar: View {
+    @Binding var noticeContent: String?
+//    var noticeContent: String = ""
+    var body: some View {
+        ZStack {
+            Color(.black)
+            
+            if let notice = noticeContent {
+                Text(notice)
+                    .foregroundColor(.white)
+            }
+            
+        }
+        .ignoresSafeArea()
+    }
+    
+
+}
+
+#Preview {
+    NoticeBar(noticeContent: .constant("러너에게 아래의 인증번호를 알려주세요"))
+//    NoticeBar()
+}
