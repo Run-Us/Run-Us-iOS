c683a47341a80a3c096ff31422da469053d62285
jikbb2 <jikbb2@gmail.com>
2024-09-16 17:23:00 +0900

    ✨ Feat: 같이 달리기 버튼 & 혼자 달리기 버튼 추가

--- RunUs/RunUs/View/RunningPage.swift
+++ RunUs/RunUs/View/RunningPage.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct RunningPage: View {
+    @State var showCreateGroupRunPage = false
     var body: some View {
-        Text("This is RunningPage")
+        ZStack {
+            VStack {
+                Button(action: {
+                    showCreateGroupRunPage = true
+                    print("click to group run button : " , showCreateGroupRunPage)
+                }, label: {
+                    Text("같이 달리기")
+                        .foregroundColor(.white)
+                        .padding()
+                })
+                .background(.blue)
+                .cornerRadius(10)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("혼자 달리기")
+                        .foregroundColor(.white)
+                        .padding()
+                })
+                .background(.blue)
+                .cornerRadius(10)
+            }
+        }
+        .navigationDestination(isPresented: $showCreateGroupRunPage, destination:{ StartGroupRunPage()
+        })
+        
     }
 }
 
