9b5a3947ed49fe93f695244a89a4a78b7969050e
jikbb2 <jikbb2@gmail.com>
2024-09-18 06:01:10 +0900

    💄 Update: SafeArea에 GeometryReader를 사용하여 접근하도록 변경하기

--- RunUs/RunUs/Utils/NoticeBar.swift
+++ RunUs/RunUs/Utils/NoticeBar.swift
@@ -9,24 +9,25 @@ import SwiftUI
 
 struct NoticeBar: View {
     @Binding var noticeContent: String?
-
+    
     var body: some View {
         ZStack {
             Color(.black)
-            
-            if let notice = noticeContent {
-                Text(notice)
-                    .foregroundColor(.white)
+            HStack(alignment: .center){
+                if let notice = noticeContent {
+                    Text(notice)
+                        .foregroundColor(.white)
+                }
             }
-            
         }
-        .ignoresSafeArea()
+        .frame(height: 40)
+        .ignoresSafeArea(edges: .horizontal)
     }
     
-
+    
 }
 
 #Preview {
     NoticeBar(noticeContent: .constant("러너에게 아래의 인증번호를 알려주세요"))
-
+    
 }
