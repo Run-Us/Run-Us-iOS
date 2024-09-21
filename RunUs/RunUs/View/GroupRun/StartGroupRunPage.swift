22abb5c8e418c2f292e1ebb8ba7acacc06c346a3
jikbb2 <jikbb2@gmail.com>
2024-09-22 04:19:52 +0900

    ♻️ Refactor: NavigationDestination에서 NavigationLink로 변경

--- RunUs/RunUs/View/GroupRun/StartGroupRunPage.swift
+++ RunUs/RunUs/View/GroupRun/StartGroupRunPage.swift
@@ -13,15 +13,13 @@ struct StartGroupRunPage: View {
     var body: some View {
         ZStack {
             VStack {
-                Button(action: {
-                    showCreateGroupRunPage = true
-                }, label: {
+                NavigationLink(destination: CreateGroupRunPage(noticeContent: .constant("러너에게 아래 인증번호를 알려주세요")), label: {
                     Text("그룹 생성하기")
                         .foregroundColor(.white)
                         .padding()
+                        .background(.blue)
+                        .cornerRadius(10)
                 })
-                .background(.blue)
-                .cornerRadius(10)
                 
                 Button(action: {}, label: {
                     Text("그룹 참가하기")
@@ -33,9 +31,6 @@ struct StartGroupRunPage: View {
             }
             
         }
-        .navigationDestination(isPresented: $showCreateGroupRunPage, destination:{ 
-            CreateGroupRunPage(noticeContent: .constant("러너에게 아래 인증번호를 알려주세요"))
-        })
     }
 }
 
