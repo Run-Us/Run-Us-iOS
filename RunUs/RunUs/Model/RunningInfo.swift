b37f5060eeba7c31a7090fb02bfed31c82cf64d8
slr-09 <ge12090@naver.com>
2024-09-21 16:09:07 +0900

    ✨ Feat: CoreMotion 권한 info.plist에 추가 & model 생성

---
+++ RunUs/RunUs/Model/RunningInfo.swift
@@ -0,0 +1,15 @@
+//
+//  RunningInfo.swift
+//  RunUs
+//
+//  Created by 가은 on 9/21/24.
+//
+
+import Foundation
+
+struct RunningInfo {
+    let startDate: Date
+    let endDate: Date
+    let averagePace: NSNumber?
+    let distance: NSNumber?
+}
---
+++ RunUs/RunUs/ViewModel/MotionManager.swift
@@ -0,0 +1,8 @@
+//
+//  MotionManager.swift
+//  RunUs
+//
+//  Created by 가은 on 9/21/24.
+//
+
+import Foundation
