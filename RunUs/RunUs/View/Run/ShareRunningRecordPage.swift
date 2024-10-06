6bab62227f98d90240362b9f213722c65ba4fbe7
slr-09 <ge12090@naver.com>
2024-10-06 21:49:36 +0900

    💄 Design: 피드 저장하기 페이지에 지도 올리기

--- RunUs/RunUs/View/Run/ShareRunningRecordPage.swift
+++ RunUs/RunUs/View/Run/ShareRunningRecordPage.swift
@@ -11,9 +11,17 @@ struct ShareRunningRecordPage: View {
     @StateObject var mapVM: MapViewModel
     
     var body: some View {
-        VStack {
-            Text("나의 기록을 저장하고, 크루에 공유해보세요!")
-            
+        GeometryReader { geometry in
+            VStack(spacing: 20) {
+                HStack {
+                    Text("나의 기록을 저장하고, 크루에 공유해보세요!")
+                    Spacer()
+                }
+                
+                MapPage(mapVM: mapVM)
+            }
+            .frame(width: max(geometry.size.width - 20, 0), height: max(geometry.size.width - 20, 0))
+            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
         }
         .navigationBarBackButtonHidden()
     }
