7acc08943f6b2cd914ea349ea1aff4cf6629138a
jikbb2 <jikbb2@gmail.com>
2024-10-12 03:40:10 +0900

    💄 Update: Login page is updated

--- RunUs/RunUs/View/Onboarding/LoginPage.swift
+++ RunUs/RunUs/View/Onboarding/LoginPage.swift
@@ -11,27 +11,57 @@ struct LoginPage: View {
     @StateObject var authVM: AuthViewModel = AuthViewModel()
     @State private var loginSuccess: Bool = false
     @State private var showJoinPage: Bool = false
+    let loginView: [String] = ["login_view_1", "login_view_2", "login_view_3"]
+    let loginViewTitle: [String] = ["친구와 함께 달려보세요!", "러닝 크루와 함께 소통하세요!", "캐릭터와 함께 성장하세요!"]
+    let loginViewContent: [String] = ["위치를 공유하며 그룹원과 함께 달리기를 즐겨보세요.", "크루의 세션 일정과 장소를 확인하고 기록을 공유하세요.", "달리기를 통해 나만의 캐릭터를 키우는 재미를 느껴보세요."]
+    @State var currentIndex: Int = 0
     
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading, spacing: 24) {
-                Text("Run with Us!")
-                Text("모든 러너가 즐겁게 성장하며\n함께 달릴 수 있는 러닝 크루 플랫폼")
-                    .lineSpacing(0.8)
-                Button(action: {
-                    authVM.kakaoLogin { isSuccess in
-                        showJoinPage = isSuccess
+            ZStack {
+                Color(.tone)
+                VStack(alignment: .center, spacing: 24) {
+                    Image("app_logo")
+                    TabView(selection: $currentIndex) {
+                        ForEach(Array(zip(loginView.indices, loginView)), id: \.0) { index, view in
+                            loginCard(view: view,
+                                      title: loginViewTitle[index],
+                                      content: loginViewContent[index])
+                            .tag(index)
+                        }
                     }
-                }, label: {
-                    Image("kakao_login_button")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                })
-                .fullScreenCover(isPresented: $showJoinPage) {
-                    JoinPage(loginSuccess: $loginSuccess)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .frame(height: 360)
+                    
+                    HStack {
+                        ForEach(loginView.indices, id: \.self) { index in
+                            Capsule()
+                                .foregroundColor(currentIndex == index ? .primary500 : .primary200)
+                                .frame(width: 8, height: 8)
+                        }
+                    }
+                    
+                    
+                    Button(action: {
+                        authVM.kakaoLogin { isSuccess in
+                            showJoinPage = isSuccess
+                        }
+                    }, label: {
+                        Image("kakao_login_button")
+                        
+                    })
+                    .fullScreenCover(isPresented: $showJoinPage) {
+                        JoinPage(loginSuccess: $loginSuccess)
+                    }
+                    Button(action: {
+                        
+                    }, label: {
+                        Image("apple_login_button")
+                    })
                 }
+                .padding(.horizontal, 20)
             }
-            .padding(.horizontal, 20)
+            .ignoresSafeArea()
             .onAppear {
                 if authVM.checkUserIdExists() {
                     loginSuccess = true
@@ -42,6 +72,23 @@ struct LoginPage: View {
             }
         }
     }
+    
+    @ViewBuilder
+        func loginCard(view: String, title: String, content: String) -> some View {
+            VStack {
+                Image(view)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text(title)
+                    .font(.title4_semibold)
+                    .foregroundColor(.gray900)
+                    .padding(.top, 8)
+                Text(content)
+                    .font(.body2_medium)
+                    .foregroundColor(.gray500)
+            }
+            .padding()
+        }
 }
 
 #Preview {
--- RunUs/RunUs/View/SplashView.swift
+++ RunUs/RunUs/View/SplashView.swift
@@ -11,7 +11,7 @@ struct SplashView: View {
     var body: some View {
         ZStack {
             Color(.tone)
-            Image("SplashLogo")
+            Image("app_logo")
         }
         .ignoresSafeArea()
     }
