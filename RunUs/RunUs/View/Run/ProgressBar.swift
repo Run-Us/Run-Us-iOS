0fe98e03204284afd60b6bf730698d1a1c4acd88
slr-09 <ge12090@naver.com>
2024-10-16 17:14:24 +0900

    💄 Design: 러닝 프로그레스 바 생성

---
+++ RunUs/RunUs/View/Run/ProgressBar.swift
@@ -0,0 +1,29 @@
+//
+//  ProgressBar.swift
+//  RunUs
+//
+//  Created by 가은 on 10/16/24.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    @State var progress: Double
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(.gray200)
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(.secondary)
+                    .frame(width: geometry.size.width * min(progress, 1.0)) // 현재는 1km가 max
+            }
+        }
+        .frame(height: 8)
+    }
+}
+
+#Preview {
+    ProgressBar(progress: 4.65)
+}
